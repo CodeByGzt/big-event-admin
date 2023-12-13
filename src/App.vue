814f28dfd7c7ff055070f1dab1ec74313ba52e88
CodeByGzt <1294266513@qq.com>
2023-12-13 11:25:22 +0800

    ✨feature: 项目初始化环境准备完成

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+const goList = () => {
+  // 查看详细属性
+  console.log(route, router)
+}
+</script>
+
+<template>
+  <div>
+    App
+    <button @click="goList">跳列表</button>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
+
+<style scoped></style>
