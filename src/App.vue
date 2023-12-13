b9b65ca42e02acab417f7242aa364b3986097018
CodeByGzt <1294266513@qq.com>
2023-12-13 15:26:30 +0800

    ✨feature: Pinia构建用户仓库和持久化和配置仓库统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
+import { useUserStore } from '@/stores'
 const router = useRouter()
 const route = useRoute()
 const goList = () => {
   // 查看详细属性
   console.log(route, router)
 }
+
+const userStore = useUserStore()
 </script>
 
 <template>
   <div>
     App
     <button @click="goList">跳列表</button>
+    {{ userStore.token }}
+    <button @click="userStore.setToken('dasdwadsdhajwhdfkasfghashkd')">
+      设置token
+    </button>
+    <button @click="userStore.setToken('')">退出</button>
     <el-row class="mb-4">
       <el-button>Default</el-button>
       <el-button type="primary">Primary</el-button>
